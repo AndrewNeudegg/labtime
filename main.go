47c9d3d7d6edfdb8b440bf335f4ebb2605a29622
Andrew Neudegg <you@example.com>
2019-04-13 21:58:08 +0100

    Configuration in place

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"labtime/pkg/config"
+)
 
+// main is the entry point of the application.
 func main() {
+	appConfig, err := config.Load("./config/custom/config.yml")
+	handleErr(err)
+
 	fmt.Println("Hello World!")
+	fmt.Println(appConfig)
+}
+
+// handleErr eases the use of errors.
+func handleErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
---
+++ pkg/config/config.go
@@ -0,0 +1,58 @@
+// Package config provides application support for YAML config files.
+package config
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+// Configuration stores the configuration for this app.
+type Configuration struct {
+	Instance GitlabInstance `json:"gitlab-instance"`
+}
+
+// GitlabInstance is the instance of Gitlab that will be accessed.
+type GitlabInstance struct {
+	URL         string `json:"url"`
+	Username    string `json:"username"`
+	AccessToken string `json:"access-token"`
+}
+
+// Default will generate the default configuration.
+func Default() Configuration {
+	return Configuration{
+		Instance: GitlabInstance{
+			URL:         "gitlab.somedomain.com",
+			Username:    "coder1",
+			AccessToken: "12345",
+		},
+	}
+}
+
+// Load will retrieve a configuration from a yaml file.
+func Load(path string) (*Configuration, error) {
+	fileBytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var data Configuration
+	err = yaml.Unmarshal(fileBytes, &data)
+	if err != nil {
+		return nil, err
+	}
+	return &data, nil
+}
+
+// Save will write a configuration to yaml file.
+func (c *Configuration) Save(path string) error {
+	y, err := yaml.Marshal(c)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(path, y, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
